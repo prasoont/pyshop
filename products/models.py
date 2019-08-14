6a7263ac4d591bdfd9a9b1ec9ba1e738b5b4250a
prasoon <tripathi.prasoon53@gmail.com>
2019-08-14 13:21:03 +0530

    My first django project,first commit.

---
+++ products/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.FloatField()
+    stock = models.IntegerField()
+    img_url = models.CharField(max_length=2038)
+
+
+class Offer(models.Model):
+    code = models.CharField(max_length=20)
+    description = models.CharField(max_length=255)
+    discount = models.FloatField()
+
+
